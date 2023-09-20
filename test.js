f43d1b85a2aba8b04d182297d3627e8b87c3c08b
customcommander <hello@spinjs.com>
2023-09-20 22:27:02 +0100

    small test macro refactor; define scope

--- test.js
+++ test.js
@@ -1,33 +1,43 @@
-import tape from 'tape';
+import test from 'tape';
 import {compile} from './lib.js';
 
-function test(outline, dsl, expected) {
+/*
+  testc - compiler test
+
+  A very simple test macro that takes a dsl string
+  as an input and an expected machine definition
+  as per the XState specification.
+
+  TODO: Is there a way to test that the expected
+        machine definition is semantically correct?
+*/
+function testc(outline, dsl, expected) {
   const actual = compile(dsl);
-  tape(outline, t => {
+  test(outline, t => {
     t.deepEqual(actual, expected);
     t.end();
   });
 }
 
-test( 'basic example'
+testc( 'basic example'
 
-    , `machine light {
-         initial state green {
-           TIMER => yellow
-         }
-         state yellow {
-           TIMER => red
-         }
-         state red {
-           TIMER => green
-         }
-       }`
+     , `machine light {
+          initial state green {
+            TIMER => yellow
+          }
+          state yellow {
+            TIMER => red
+          }
+          state red {
+            TIMER => green
+          }
+        }`
 
-    , { predictableActionArguments: true
-      , id: 'light'
-      , initial: 'green'
-      , states:
-        {  green: {on: {TIMER: 'yellow'}}
-        , yellow: {on: {TIMER:    'red'}}
-        ,    red: {on: {TIMER:  'green'}}}});
+     , { predictableActionArguments: true
+       , id: 'light'
+       , initial: 'green'
+       , states:
+         {  green: {on: {TIMER: 'yellow'}}
+         , yellow: {on: {TIMER:    'red'}}
+         ,    red: {on: {TIMER:  'green'}}}});
 
